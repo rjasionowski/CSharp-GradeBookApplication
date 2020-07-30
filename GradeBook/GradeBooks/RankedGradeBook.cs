a269c65fb7d958b8a750ce3ecc9428c325c80be3
Y-PRIME\rjasionowski <rjasionowski@yprime.com>
2020-07-30 17:22:23 -0400

    no message

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,74 +19,28 @@ namespace GradeBook.GradeBooks
             this.Type = GradeBookType.Ranked;
         }
 
-        public void FillAndStratifyGrades()
-        {
-            foreach (Student student in this.Students)
-            {
-                classGrades.Add(student.AverageGrade);
-            }
-            classGrades.Sort();
-
-            double tempPercentile = 0.8;
-            double tempIndex = 0.0;
-            double tempCutoff = 0.0;
-            
-            while(tempPercentile > 0)
-            {
-                tempIndex = classGrades.Count() * tempPercentile;
-                if(((int)tempIndex).Equals(tempIndex))
-                {
-                    tempCutoff = (classGrades[(int)tempIndex] + classGrades[(int)tempIndex + 1]) / 2;
-                }
-                else
-                {
-                    tempIndex += 1;
-                    tempCutoff = (classGrades[(int)tempIndex]);
-                }
-                if(tempPercentile == 0.8)
-                {
-                    aCutoff = tempCutoff;
-                }
-                else if(tempPercentile == 0.6)
-                {
-                    bCutoff = tempCutoff;
-                }
-                else if(tempPercentile == 0.4)
-                {
-                    cCutoff = tempCutoff;
-                }
-                else if(tempPercentile == 0.2)
-                {
-                    dCutOff = tempCutoff;
-                }
-                tempPercentile -= 0.2;
-            }
-        }
-
         public override char GetLetterGrade(double averageGrade)
         {
             if (this.Students.Count < 5)
             {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
-            if (classGrades.Count() == 0)
-            {
-                FillAndStratifyGrades();
-            }
-            
-            if(averageGrade > aCutoff)
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if(grades[threshold-1] <= averageGrade)
             {
                 return 'A';
             }
-            else if(averageGrade > bCutoff)
-            {
+            else if(grades[(threshold * 2) - 1] <= averageGrade){
                 return 'B';
             }
-            else if(averageGrade > cCutoff)
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
             {
                 return 'C';
             }
-            else if(averageGrade > dCutOff)
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
             {
                 return 'D';
             }
@@ -94,8 +48,6 @@ namespace GradeBook.GradeBooks
             {
                 return 'F';
             }
-
         }
-
     }
 }
