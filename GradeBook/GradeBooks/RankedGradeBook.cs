8ac486ebc6e1aaf25d5e99a87106b535eb7c2e78
Y-PRIME\rjasionowski <rjasionowski@yprime.com>
2020-07-30 14:36:04 -0400

    no message

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+
+        public RankedGradeBook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Ranked;
+        }
+    }
+}
